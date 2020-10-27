f7f50917a07e4317753a5a10a8fd7a253064142c
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-10-27 16:02:03 -0300

    Add files via upload

---
+++ src/voting.js
@@ -0,0 +1,12 @@
+const vote = {
+    typeB: () => {
+        // !voting o que beber? .água .café .chá
+        
+        // !voting o que beber? .água .café 
+        // !voting euaheau .sim .nao
+        
+
+    
+    
+    }
+}
