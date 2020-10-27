7581d443e41a9c43fd97a56b10694082da9c309a
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-10-27 16:03:02 -0300

    Update voting.js

--- src/voting.js
+++ src/voting.js
@@ -1,12 +1,5 @@
 const vote = {
     typeB: () => {
-        // !voting o que beber? .água .café .chá
-        
-        // !voting o que beber? .água .café 
-        // !voting euaheau .sim .nao
-        
-
-    
     
     }
 }
