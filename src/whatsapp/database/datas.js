db4285c0423307621c5c675cfd21e7870880ec21
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-11-01 21:12:05 -0300

    Update datas.js

--- src/whatsapp/database/datas.js
+++ src/whatsapp/database/datas.js
@@ -1,11 +1,12 @@
 const Database = require("./db");
 const register = require("./register")
-const datasRegister = {from: undefined, name: undefined, all_permictions: undefined, value_verify: 0};
+const datasRegister = {group: undefined, number: undefined, name: undefined, all_permictions: undefined, value_verify: 0};
 
 let tableUsers;
                 
-function check(from, name, all_permictions){
-    datasRegister.from = from;
+function check(from, number, name, all_permictions){
+    datasRegister.group = from;
+    datasRegister.number = number;
     datasRegister.name = name;
     datasRegister.all_permictions = all_permictions;
 
@@ -16,18 +17,26 @@ function check(from, name, all_permictions){
             tableUsers =  await db.all("SELECT * FROM usering");
             console.log(tableUsers.length)
         })
-        .then( (from = datasRegister.from, i = 0) => {
+    // Conferir se o number do usuário já está cadastrado
+        .then( (group = datasRegister.group, number = datasRegister.number, i = 0) => {
+            console.log(tableUsers);
             // Verificar se o usuário já existe:
             while(i < tableUsers.length){
-                if(tableUsers[i].number === datasRegister.from){
+                if(tableUsers[i].number === number && tableUsers[i].groupId === group){
                     datasRegister.value_verify = 1;
                     break;
                 } else datasRegister.value_verify = 0;
                 ++i;
             }
+            console.log("value_verify = " + datasRegister.value_verify);
         if(datasRegister.value_verify === 0) {
-            register(datasRegister.from, datasRegister.name, datasRegister.all_permictions);
-        } else console.log("The user already exists");
+            console.log("from (group) = " + datasRegister.group);
+            console.log("from (user) = " + datasRegister.number);
+            register(datasRegister.group, datasRegister.number, datasRegister.name, datasRegister.all_permictions);
+        } else {
+            datasRegister.value_verify = 0;
+            console.log("The user already exists")
+        }
     })     
 }    
 
