fd016a01211f04a503a8d9b673879f3866d07305
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-11-06 19:12:03 -0300

    Add files via upload

---
+++ src/whatsapp/help.js
@@ -0,0 +1,21 @@
+const help = 
+    `
+        < Recursos do bot: >
+
+        _*!help* -> mostrar a lista de_
+        _comandos e suas funções;_
+        
+        _*!register* -> fazer a inscrição para_
+        _poder usar todos os comandos_
+        _do bot_
+            _(para ser marcado no *!all*, você_
+                _*precisará* estar cadastrado)_
+        
+        _*!all* -> marcar todos os membros_
+        _do grupo *(devem* *estar*_ 
+                     _*cadastrados*);_
+
+    `
+
+
+module.exports = help;
