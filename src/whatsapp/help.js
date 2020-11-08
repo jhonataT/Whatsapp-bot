42db90a85c963c31968c1668c8e3af7e8537a68e
Jhonata Tenório <51134324+JhN-Te@users.noreply.github.com>
2020-11-07 22:22:44 -0300

    Update help.js

--- src/whatsapp/help.js
+++ src/whatsapp/help.js
@@ -2,18 +2,34 @@ const help =
     `
         < Recursos do bot: >
 
-        _*!help* -> mostrar a lista de_
-        _comandos e suas funções;_
+        _*~!help~ -> mostrar a lista de*_
+        _*comandos e suas funções;*_
         
-        _*!register* -> fazer a inscrição para_
-        _poder usar todos os comandos_
-        _do bot_
-            _(para ser marcado no *!all*, você_
-                _*precisará* estar cadastrado)_
+        _*~!register~ -> fazer a inscrição para*_
+        _*poder usar todos os comandos*_
+        _*do bot*_
+            _*(para ser marcado no ~!all~, você*_
+                _*precisará estar cadastrado)*_
         
-        _*!all* -> marcar todos os membros_
-        _do grupo *(devem* *estar*_ 
-                     _*cadastrados*);_
+        _*~!all~ -> marcar todos os membros*_
+        _*do grupo (devem* *estar*_ 
+                     _*cadastrados);*_
+        
+    _*~!voting~ tema #opção_1 #opção_2*_
+
+    _*Iniciar uma votação:*_
+    
+    _*( O sistema de votação é simples,*_
+    _*você só precisa irformar o*_
+    _*comando ( !voting ),*_
+    _*seguido do tema da votação e das*_ 
+    _*opções que podem ser votadas)*_
+
+    _*Votar em uma opção:*_
+    _*para votar, basta informar*_
+    _*o comando: ~!vote~ n, onde "n"*_
+    _*é o número da opção.*_
+
 
     `
 
