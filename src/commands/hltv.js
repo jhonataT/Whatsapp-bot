2f54608e7e8c75a17def446804dc588821174573
Jhn <jhonata.tenorio2526@gmail.com>
2021-08-19 19:02:12 -0300

    Retornando projeto

--- src/commands/hltv.js
+++ src/commands/hltv.js
@@ -4,7 +4,7 @@ class Csgo {
     static  async teamStats(client, message, teamName){
         let sts = await HLTV.getResults({startPage:0,endPage:1}).then((res) => {
             for(let i = 0; i < res.length; i++){
-                if(res[i].team1.name.toLowerCase() === teamName || res[i].team2.name.toLowerCase() === teamName){
+                if(res[i].team1?.name.toLowerCase() === teamName || res[i].team2?.name.toLowerCase() === teamName){
                     return res[i];
                     break;
                 }
@@ -21,10 +21,10 @@ class Csgo {
             for(let i = 0; i < res.length; i++){
                 if(res[i].live === true){
                   livesInfos = livesInfos.concat(`
-            *${res[i].team1.name} x ${res[i].team2.name}*\n`);
+            *${res[i].team1?.name} x ${res[i].team2?.name}*\n`);
             
                 livesInfos = livesInfos.concat(`*evento:*\n`) 
-                livesInfos = livesInfos.concat(`_${res[i].event.name}_\n`);
+                livesInfos = livesInfos.concat(`_${res[i].event?.name}_\n`);
                 livesInfos = livesInfos.concat(`*formato: ${res[i].format}*\n`);
               }
             }
@@ -40,7 +40,7 @@ class Csgo {
         let live = false;
         for(let i = 0; i < infos.length; i++){
             if(infos[i].live === true){
-                if(infos[i].team1.name.toLocaleLowerCase() === teamName || infos[i].team2.name.toLocaleLowerCase() === teamName){
+                if(infos[i].team1?.name.toLocaleLowerCase() === teamName || infos[i].team2?.name.toLocaleLowerCase() === teamName){
                     live = true;
                     const matchInfo = await HLTV.getMatch( {id: infos[i].id} );
                     console.log(matchInfo);
@@ -51,23 +51,23 @@ class Csgo {
 *SOBRE O JOGO DO/DA ${teamName}:*
 *status:* _${matchInfo.status}_
 
-    _*${matchInfo.team1.name} X ${matchInfo.team2.name}*_
+    _*${matchInfo.team1?.name} X ${matchInfo.team2?.name}*_
     
 *Formato:* _${matchInfo.format}._
 
 *Evento:*
 
-_${matchInfo.event.name}._
+_${matchInfo.event?.name}._
 
 *Mapas:*
 
-*${matchInfo.maps[0].name}:*
+*${matchInfo.maps[0]?.name}:*
 *_${matchInfo.maps[0].result}_*
 
-*${matchInfo.maps[1].name}:*
+*${matchInfo.maps[1]?.name}:*
 *_${matchInfo.maps[1].result}_*
 
-*${matchInfo.maps[2].name}:*
+*${matchInfo.maps[2]?.name}:*
 *_${matchInfo.maps[2].result}_*
 
 
@@ -90,7 +90,7 @@ async function infoCsgoTeams(teamName, sts, client, message) {
     let maps = "*Mapas e seus resultados:*\n";
     for(let i = 0; i < moreInfos.maps.length; i++){
       maps = maps.concat(`
-      *${moreInfos.maps[i].name},* 
+      *${moreInfos.maps[i]?.name},* 
       *result: ${moreInfos.maps[i].result}*\n
       `);
     }
@@ -99,7 +99,7 @@ async function infoCsgoTeams(teamName, sts, client, message) {
   *SOBRE O ÚLTIMO JOGO DO TIME ${teamName}:*
                 
   _TIMES:_
-  _*${sts.team1.name} x ${sts.team2.name}*_
+  _*${sts.team1?.name} x ${sts.team2.name}*_
   
   _*Formato: ${moreInfos.format}*_
     
@@ -108,10 +108,10 @@ async function infoCsgoTeams(teamName, sts, client, message) {
   ${maps}
   
   _Player da partida:_
-  *${moreInfos.highlightedPlayer.name}*
+  *${moreInfos.highlightedPlayer?.name}*
   
   
-  _TORNEIO: ${sts.event.name}_
+  _TORNEIO: ${sts.event?.name}_
 
     `);
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const wa = require('@open-wa/wa-automate');
-const join = require('./modules/joinGroup');
+// const join = require('./modules/joinGroup');
 const all = require('./commands/all');
-const db = require('./modules/db');
+// const db = require('./modules/db');
 const IsOnline = require('./commands/isOnline');
 const Adm = require('./commands/admin');
 const hltv = require('./commands/hltv');
@@ -13,12 +13,6 @@ let numberFile = 0, timeRestart = false;
 wa.create({ authTimeout: 120 }).then(client => init(client));
 
 const init = async (client) => {
-  setInterval( async () => {
-    console.log('LOOKING FOR A NEW GROUP!')
-    await join(client);
-    console.log('All ok!!')
-    await client.sendText(process.env.ADM_NUMBER, 'All ok!');
-  }, 180000);
 
   client.onMessage(async (message) => {
     await client.sendSeen(message.from);
@@ -39,37 +33,26 @@ const GroupsMessage = async (client, message) => {
   .substring(PREFIX.length)
   .split(/\s+/);
 
-  const links = await db.dataTable();
-
-  links.forEach( data => {
-    if(data.groupId === message.from){
-      data.cmds = data.cmds.toLowerCase();
-      console.log(`${message.sender.pushname}: ${CMD_NAME}`);
-      console.table(data);
-      const adm = new Adm(client, message, args);
-        if(data.cmds.indexOf(CMD_NAME) != -1){
-          if(CMD_NAME === 'help'|| CMD_NAME === 'h')
-            all.help(client, message);
-          if(CMD_NAME === 'all' || CMD_NAME === 'a')
-            all.mention(client, message);
-          if(CMD_NAME === 'online' || CMD_NAME === 'on')
-            IsOnline.on(client, message);
-          if(CMD_NAME === 'adm' || CMD_NAME === 'ad')
-            adm.mention();
-          if(CMD_NAME === 'live' || CMD_NAME === 'l')
-            hltv.live(client, message);
-          if(args.length != 0) {
-            if(CMD_NAME === 'promote' || CMD_NAME === 'p')
-              adm.promoteUser();
-            if(CMD_NAME === 'denote' || CMD_NAME === 'd')
-              adm.denoteUser();
-            if(CMD_NAME === 'team' || CMD_NAME === 't')
-              hltv.liveInfo(client, message, args.toString().replace(/,/gi, " "));
-          }
-        }
-        else console.log(`${data.groupId} não tem permissão para usar o comando: ${CMD_NAME}`);
-      }
-  });
+  console.log(`${message.sender.pushname}: ${CMD_NAME}`);
+  const adm = new Adm(client, message, args);
+    if(CMD_NAME === 'help'|| CMD_NAME === 'h')
+      all.help(client, message);
+    if(CMD_NAME === 'all' || CMD_NAME === 'a')
+      all.mention(client, message);
+    if(CMD_NAME === 'online' || CMD_NAME === 'on')
+      IsOnline.on(client, message);
+    if(CMD_NAME === 'adm' || CMD_NAME === 'ad')
+      adm.mention();
+    if(CMD_NAME === 'live' || CMD_NAME === 'l')
+      hltv.live(client, message);
+    if(args.length != 0) {
+      if(CMD_NAME === 'promote' || CMD_NAME === 'p')
+        adm.promoteUser();
+      if(CMD_NAME === 'denote' || CMD_NAME === 'd')
+        adm.denoteUser();
+      // if(CMD_NAME === 'team' || CMD_NAME === 't')
+      //   hltv.liveInfo(client, message, args.toString().replace(/,/gi, " "));
+    }
 };
 
 const sendSticker = (client, message) => {
@@ -79,7 +62,7 @@ const sendSticker = (client, message) => {
   .trim()
   .substring(PREFIX.length)
   .split(/\s+/);
-  console.log(`${message.sender.pushname}: ${CMD_NAME}`);
+  // console.log(`${message.sender.pushname}: ${CMD_NAME}`);
   if(message.type === 'image'){
     console.log(CMD_NAME);
     if(CMD_NAME === "sticker" || CMD_NAME === "s")
